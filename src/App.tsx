0c61b3920d298d6b93c3a630f23b76b768f739a5
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-01-18 12:28:04 +0200

    created the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
 function App() {
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
 
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="app bg-gray-20">
+      <Navbar 
+        isTopOfPage = {isTopOfPage}
+        selectedPage = {selectedPage}
+        setSelectedPage ={setSelectedPage}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
