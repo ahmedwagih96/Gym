451bdf93cc7512c1589cf95203e2a37df130add1
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-01-19 21:41:59 +0200

    created the contact us section

---
+++ src/scenes/contact/Form.tsx
@@ -0,0 +1,77 @@
+import { useForm } from "react-hook-form";
+
+
+function Form() {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white outline-none`;
+  const {register,trigger,formState: { errors },} = useForm();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+  return (
+    <form
+      action="https://formsubmit.co/dd37aab76abaa3eb5a1701516652eb5a"
+      target="_blank"
+      onSubmit={handleSubmit}
+      method="POST"
+      className=""
+    >
+      <input
+        type="text"
+        className={inputStyles}
+        placeholder="NAME"
+        {...register("name", {
+          required: true,
+          maxLength: 100,
+        })}
+      />
+      {errors.name && (
+        <p className="mt-1 text-primary-500">
+          {errors.name.type === "required" && "This field is required"}
+          {errors.name.type === "maxLength" && "Max length is 100 char."}
+        </p>
+      )}
+      <input
+        type="text"
+        className={inputStyles}
+        placeholder="EMAIL"
+        {...register("email", {
+          required: true,
+          pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        })}
+      />
+      {errors.email && (
+        <p className="mt-1 text-primary-500">
+          {errors.email.type === "required" && "This field is required"}
+          {errors.email.type === "pattern" && "Invalid email address"}
+        </p>
+      )}
+      <textarea
+        rows={4}
+        cols={50}
+        className={inputStyles}
+        placeholder="MESSAGE"
+        {...register("message", {
+          required: true,
+          maxLength: 2000,
+        })}
+      />
+      {errors.message && (
+        <p className="mt-1 text-primary-500">
+          {errors.message.type === "required" && "This field is required"}
+          {errors.message.type === "maxLength" && "Max length is 2000 char."}
+        </p>
+      )}
+      <button
+        type="submit"
+        className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
--- src/scenes/contact/index.tsx
+++ src/scenes/contact/index.tsx
@@ -1,12 +1,75 @@
+import HText from "@/shared/HText";
 import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { setSelectedPage } from "@/features/selectedPageSlice";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import Form from "./Form";
 
-type Props = {};
-
-function Contact({}: Props) {
+function Contact() {
+  const dispatch = useDispatch();
   return (
-    <section id={SelectedPage.ContactUs}>
-      <div></div>
-      Contact
+    <section id={SelectedPage.ContactUs} className="pb-22 mx-auto w-5/6 pt-32">
+      <motion.div
+        onViewportEnter={() =>
+          dispatch(setSelectedPage(SelectedPage.ContactUs))
+        }
+      >
+        {/* HEADING */}
+        <motion.div
+          initial="hidden"
+          viewport={{ once: true, amount: 0.5 }}
+          whileInView="visible"
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">join now</span> get in shape
+          </HText>
+          <p className="my-5"></p>
+        </motion.div>
+
+        {/* FORM AND DIV */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          {/* FORM */}
+          <motion.div
+            initial="hidden"
+            viewport={{ once: true, amount: 0.5 }}
+            whileInView="visible"
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="mt-10 basis-3/5 md:mt-0"
+          >
+            <Form />
+          </motion.div>
+          {/* IMAGE */}
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            viewport={{ once: true, amount: 0.5 }}
+            whileInView="visible"
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                src={ContactUsPageGraphic}
+                alt="contact-us-page-graphic"
+                className="w-full"
+              />
+            </div>
+          </motion.div>
+        </div>
+      </motion.div>
     </section>
   );
 }
